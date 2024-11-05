aab94fb5691e6ab32ef5d623e9244b61083a0d15
alekodu <alekodu@gmail.com>
2024-11-05 09:44:39 +0000

    [WIP] adding ids and status to logs

--- internal/control_test_engine/ue/gtp/service/service.go
+++ internal/control_test_engine/ue/gtp/service/service.go
@@ -25,18 +25,18 @@ func SetupGtpInterface(ue *context.UEContext, msg gnbContext.UEMessage) {
 	gnbPduSession := msg.GNBPduSessions[0]
 	pduSession, err := ue.GetPduSession(uint8(gnbPduSession.GetPduSessionId()))
 	if pduSession == nil || err != nil {
-		log.Error("[GNB][GTP] Aborting the setup of PDU Session ", gnbPduSession.GetPduSessionId(), ", this PDU session was not succesfully configured on the UE's side.")
+		log.Error("[GNB][GTP][]()()(", ue.GetPrUeId(), ") Aborting the setup of PDU Session ", gnbPduSession.GetPduSessionId(), ", this PDU session was not succesfully configured on the UE's side.")
 		return
 	}
 	pduSession.GnbPduSession = gnbPduSession
 
 	if ue.TunnelMode == config.TunnelDisabled {
-		log.Info(fmt.Sprintf("[UE][GTP] Interface for UE %s has not been created. Tunnel has been disabled.", ue.GetMsin()))
+		log.Info(fmt.Sprintf("[UE][GTP][]()()(", ue.GetMsin(), ")Interface for UE %s has not been created. Tunnel has been disabled.", ue.GetMsin()))
 		return
 	}
 
 	if pduSession.Id != 1 {
-		log.Warn("[GNB][GTP] Only one tunnel per UE is supported for now, no tunnel will be created for second PDU Session of given UE")
+		log.Warn("[GNB][GTP][]()()(", ue.GetPrUeId(), ") Only one tunnel per UE is supported for now, no tunnel will be created for second PDU Session of given UE")
 		return
 	}
 
@@ -61,7 +61,7 @@ func SetupGtpInterface(ue *context.UEContext, msg gnbContext.UEMessage) {
 	go func() {
 		// This function should not return as long as the GTP-U UDP socket is open
 		if err := gtpLink.CmdAdd(nameInf, 1, ueGnbIp.String(), stopSignal); err != nil {
-			log.Fatal("[GNB][GTP] Unable to create Kernel GTP interface: ", err, msin, nameInf)
+			log.Fatal("[GNB][GTP][]()()(", ue.GetPrUeId(), ") Unable to create Kernel GTP interface: ", err, msin, nameInf)
 			return
 		}
 	}()
@@ -71,31 +71,31 @@ func SetupGtpInterface(ue *context.UEContext, msg gnbContext.UEMessage) {
 	time.Sleep(time.Second)
 
 	cmdAddFar := []string{nameInf, "1", "--action", "2"}
-	log.Debug("[UE][GTP] Setting up GTP Forwarding Action Rule for ", strings.Join(cmdAddFar, " "))
+	log.Debug("[UE][GTP][]()()(", ue.GetMsin(), ")Setting up GTP Forwarding Action Rule for ", strings.Join(cmdAddFar, " "))
 	if err := gtpTunnel.CmdAddFAR(cmdAddFar); err != nil {
-		log.Fatal("[GNB][GTP] Unable to create FAR: ", err)
+		log.Fatal("[GNB][GTP][]()()(", ue.GetPrUeId(), ") Unable to create FAR: ", err)
 		return
 	}
 
 	cmdAddFar = []string{nameInf, "2", "--action", "2", "--hdr-creation", "0", fmt.Sprint(gnbPduSession.GetTeidUplink()), upfIp, "2152"}
-	log.Debug("[UE][GTP] Setting up GTP Forwarding Action Rule for ", strings.Join(cmdAddFar, " "))
+	log.Debug("[UE][GTP][]()()(", ue.GetMsin(), ")Setting up GTP Forwarding Action Rule for ", strings.Join(cmdAddFar, " "))
 	if err := gtpTunnel.CmdAddFAR(cmdAddFar); err != nil {
-		log.Fatal("[UE][GTP] Unable to create FAR ", err)
+		log.Fatal("[UE][GTP][]()()(", ue.GetMsin(), ")Unable to create FAR ", err)
 		return
 	}
 
 	cmdAddPdr := []string{nameInf, "1", "--pcd", "1", "--hdr-rm", "0", "--ue-ipv4", ueIp, "--f-teid", fmt.Sprint(gnbPduSession.GetTeidDownlink()), msg.GnbIp, "--far-id", "1"}
-	log.Debug("[UE][GTP] Setting up GTP Packet Detection Rule for ", strings.Join(cmdAddPdr, " "))
+	log.Debug("[UE][GTP][]()()(", ue.GetMsin(), ")Setting up GTP Packet Detection Rule for ", strings.Join(cmdAddPdr, " "))
 
 	if err := gtpTunnel.CmdAddPDR(cmdAddPdr); err != nil {
-		log.Fatal("[GNB][GTP] Unable to create FAR: ", err)
+		log.Fatal("[GNB][GTP][]()()(", ue.GetPrUeId(), ") Unable to create FAR: ", err)
 		return
 	}
 
 	cmdAddPdr = []string{nameInf, "2", "--pcd", "2", "--ue-ipv4", ueIp, "--far-id", "2"}
-	log.Debug("[UE][GTP] Setting Up GTP Packet Detection Rule for ", strings.Join(cmdAddPdr, " "))
+	log.Debug("[UE][GTP][]()()(", ue.GetMsin(), ")Setting Up GTP Packet Detection Rule for ", strings.Join(cmdAddPdr, " "))
 	if err := gtpTunnel.CmdAddPDR(cmdAddPdr); err != nil {
-		log.Fatal("[UE][GTP] Unable to create FAR ", err)
+		log.Fatal("[UE][GTP][]()()(", ue.GetMsin(), ")Unable to create FAR ", err)
 		return
 	}
 
@@ -112,7 +112,7 @@ func SetupGtpInterface(ue *context.UEContext, msg gnbContext.UEMessage) {
 	pduSession.SetTunInterface(link)
 
 	if err := netlink.AddrAdd(link, addrTun); err != nil {
-		log.Fatal("[UE][DATA] Error in adding IP for virtual interface", err)
+		log.Fatal("[UE][DATA][]()()(", ue.GetMsin(), ")Error in adding IP for virtual interface", err)
 		return
 	}
 
@@ -127,7 +127,7 @@ func SetupGtpInterface(ue *context.UEContext, msg gnbContext.UEMessage) {
 		_ = netlink.RuleDel(rule)
 
 		if err := netlink.RuleAdd(rule); err != nil {
-			log.Fatal("[UE][DATA] Unable to create routing policy rule for UE", err)
+			log.Fatal("[UE][DATA][]()()(", ue.GetMsin(), ")Unable to create routing policy rule for UE", err)
 			return
 		}
 		pduSession.SetTunRule(rule)
@@ -141,17 +141,17 @@ func SetupGtpInterface(ue *context.UEContext, msg gnbContext.UEMessage) {
 		_ = netlink.LinkDel(vrfDevice)
 
 		if err := netlink.LinkAdd(vrfDevice); err != nil {
-			log.Fatal("[UE][DATA] Unable to create VRF for UE", err)
+			log.Fatal("[UE][DATA][]()()(", ue.GetMsin(), ")Unable to create VRF for UE", err)
 			return
 		}
 
 		if err := netlink.LinkSetMaster(link, vrfDevice); err != nil {
-			log.Fatal("[UE][DATA] Unable to set GTP tunnel as slave of VRF interface", err)
+			log.Fatal("[UE][DATA][]()()(", ue.GetMsin(), ")Unable to set GTP tunnel as slave of VRF interface", err)
 			return
 		}
 
 		if err := netlink.LinkSetUp(vrfDevice); err != nil {
-			log.Fatal("[UE][DATA] Unable to set interface VRF UP", err)
+			log.Fatal("[UE][DATA][]()()(", ue.GetMsin(), ")Unable to set interface VRF UP", err)
 			return
 		}
 		pduSession.SetVrfDevice(vrfDevice)
@@ -167,17 +167,17 @@ func SetupGtpInterface(ue *context.UEContext, msg gnbContext.UEMessage) {
 	}
 
 	if err := netlink.RouteReplace(route); err != nil {
-		log.Fatal("[GNB][GTP] Unable to create Kernel Route ", err)
+		log.Fatal("[GNB][GTP][]()()(", ue.GetPrUeId(), ") Unable to create Kernel Route ", err)
 	}
 	pduSession.SetTunRoute(route)
 
-	log.Info(fmt.Sprintf("[UE][GTP] Interface %s has successfully been configured for UE %s", nameInf, ueIp))
+	log.Info(fmt.Sprintf("[UE][GTP][]()()(", ue.GetMsin(), ")Interface %s has successfully been configured for UE %s", nameInf, ueIp))
 	switch ue.TunnelMode {
 	case config.TunnelTun:
-		log.Info(fmt.Sprintf("[UE][GTP] You can do traffic for this UE by binding to IP %s, eg:", ueIp))
-		log.Info(fmt.Sprintf("[UE][GTP] iperf3 -B %s -c IPERF_SERVER -p PORT -t 9000", ueIp))
+		log.Info(fmt.Sprintf("[UE][GTP][]()()(", ue.GetMsin(), ")You can do traffic for this UE by binding to IP %s, eg:", ueIp))
+		log.Info(fmt.Sprintf("[UE][GTP][]()()(", ue.GetMsin(), ")iperf3 -B %s -c IPERF_SERVER -p PORT -t 9000", ueIp))
 	case config.TunnelVrf:
-		log.Info(fmt.Sprintf("[UE][GTP] You can do traffic for this UE using VRF %s, eg:", vrfInf))
-		log.Info(fmt.Sprintf("[UE][GTP] sudo ip vrf exec %s iperf3 -c IPERF_SERVER -p PORT -t 9000", vrfInf))
+		log.Info(fmt.Sprintf("[UE][GTP][]()()(", ue.GetMsin(), ")You can do traffic for this UE using VRF %s, eg:", vrfInf))
+		log.Info(fmt.Sprintf("[UE][GTP][]()()(", ue.GetMsin(), ")sudo ip vrf exec %s iperf3 -c IPERF_SERVER -p PORT -t 9000", vrfInf))
 	}
 }
