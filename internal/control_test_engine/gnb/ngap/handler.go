aafa0c5b6dd6f50b01ed9ffba1a0fe0e2bf0bcb7
Aleksandra Obeso Duque <alekodu@gmail.com>
2024-11-15 11:24:48 +0100

    [WIP] adding ids and status to logs

--- internal/control_test_engine/gnb/ngap/handler.go
+++ internal/control_test_engine/gnb/ngap/handler.go
@@ -745,6 +745,9 @@ func HandlerUeContextReleaseCommand(gnb *context.GNBContext, message *ngapType.N
 		return
 	}
 
+	ue.SetProcedureType(string(context.UE_DETTACH))
+	ue.SetProcedureStage(string(context.INITIATED))
+
 	logFields[misc.PROCEDURE] = ue.GetProcedureType()
 	logFields[misc.STAGE] = ue.GetProcedureStage()
 	logFields[misc.UE_PR_ID] = ue.GetPrUeId()
